776920d075a057d9830a86c323a03527526e9b03
jared <jtlss14@gmail.com>
2020-11-12 11:00:34 +0000

    task 7

--- task.py
+++ task.py
@@ -21,3 +21,39 @@ shop_dict = {1:"bread", 2:"bananas", 3:"apples"}
 for key, value in shop_dict.items():
     print(f"{key} : {value}")
     print(type(value))
+
+
+print("*"*30)
+# Task 4
+# Replace the price of items in Jake's code
+jake_shop_list = {"apples":4, "books":6, "bananas":2}
+jake_shop_list["apples"] = 3
+print(jake_shop_list["apples"])
+
+
+print("*"*30)
+# Task 5
+# Make an add function
+def add(num1, num2):
+    return num1 + num2
+
+print(add(2, 4))
+
+print("*"*30)
+# Task 6
+# Create a Person class, with name and age as parameters
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+# Task 7
+# Create a subclass of Person called Student
+class Student(Person):
+    def __init__(self, name, age, student_id, course):
+        super().__init__(name, age)
+        self.student_id = student_id
+        self.course = course
+
+j = Student("Jared", 21, 123, "DevOps")
+print(j.name, j.age, j.student_id, j.course)
