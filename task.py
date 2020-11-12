2d4e17fbd75672deb722742f82e6396bde9613cf
jared <jtlss14@gmail.com>
2020-11-12 10:32:52 +0000

    task 2

--- task.py
+++ task.py
@@ -1,7 +1,16 @@
 # Task 1
 # Declare a list with numbers 1 to 5 and add 6 at the end of the list
-list = [1, 2, 3, 4, 5]
-list.append(6)
-print(list)
+nlist = [1, 2, 3, 4, 5]
+nlist.append(6)
+print(nlist)
 
+print("*"*30)
 # Task 2
+# Create a tuple with values 1 to 5, insert 2.2 between 2 and 3, 
+nums = {1, 2, 3, 4, 5}
+
+for _ in nums:
+    if _ > 3:
+        break
+    print(_)
+
