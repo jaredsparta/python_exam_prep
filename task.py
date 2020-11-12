e9ff230987312f2c87ccab20551602e653dff143
jared <jtlss14@gmail.com>
2020-11-12 10:33:16 +0000

    task 2

--- task.py
+++ task.py
@@ -6,7 +6,7 @@ print(nlist)
 
 print("*"*30)
 # Task 2
-# Create a tuple with values 1 to 5, insert 2.2 between 2 and 3, 
+# Create a tuple with values 1 to 5 then iterate through and rint until 3 then stop
 nums = {1, 2, 3, 4, 5}
 
 for _ in nums:
