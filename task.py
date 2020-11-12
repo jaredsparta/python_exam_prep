79f416959d7560411cf3b2258a4e9be5dbed18bc
jared <jtlss14@gmail.com>
2020-11-12 10:47:01 +0000

    task 3

--- task.py
+++ task.py
@@ -6,7 +6,7 @@ print(nlist)
 
 print("*"*30)
 # Task 2
-# Create a tuple with values 1 to 5 then iterate through and rint until 3 then stop
+# Create a tuple with values 1 to 5 then iterate through and print until 3 then stop
 nums = {1, 2, 3, 4, 5}
 
 for _ in nums:
@@ -14,3 +14,10 @@ for _ in nums:
         break
     print(_)
 
+print("*"*30)
+# Task 3
+# Declare a shopping list dictionary
+shop_dict = {1:"bread", 2:"bananas", 3:"apples"}
+for key, value in shop_dict.items():
+    print(f"{key} : {value}")
+    print(type(value))
